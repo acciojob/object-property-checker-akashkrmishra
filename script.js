d30d0eb20f9307dd9cf9a46dd023ffbc9408e0e8
akashkrmishra <142215838+akashkrmishra@users.noreply.github.com>
2023-11-02 15:36:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 function hasKey(key) {
   // Check if the key exists in sampleObject
   if (sampleObject.hasOwnProperty(key)) {
-    // If it does, return true
-	  if (sampleObject==red ||sampleObject==white) {
+	  if (sampleObject=="red" ||sampleObject=="white") {
 	  	return true;
 	  }
     
   } else {
-    // If it doesn't, return false
     return false;
   }
 }
